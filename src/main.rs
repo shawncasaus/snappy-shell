2eb612495f3989e00914d6fdfdfee46f1b62e909
shawncasaus <shawn.casaus@gmail.com>
2026-03-15 16:17:47 +0000

    Added file execution to shell.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use path_utils::find_executables;
 use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::process::CommandExt;
+use std::process::Command;
 
 fn main() {
     let type_map = HashMap::from([
@@ -19,22 +21,36 @@ fn main() {
         io::stdin().read_line(&mut command).unwrap();
 
         command = command.trim().to_string();
-        if command == "exit" {
+        let parts: Vec<&str> = command.split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
+        }
+        let cmd = parts[0];
+        let args = &parts[1..];
+
+        if cmd == "exit" {
             break;
-        } else if command.starts_with("type") {
-            let input_type = &command[5..];
-            if type_map.contains_key(input_type) {
-                println!("{} is a shell builtin", input_type);
-            } else if let Some(path) = find_executables(input_type) {
-                println!("{} is {}", input_type, path);
+        } else if cmd == "type" {
+            if args.is_empty() {
+                println!("{}: no arguments provided", cmd);
             } else {
-                println!("{}: not found", input_type);
+                let input_type = &args[0];
+                if type_map.contains_key(input_type) {
+                    println!("{} is a shell builtin", input_type);
+                } else if let Some(path) = find_executables(input_type) {
+                    println!("{} is {}", input_type, path);
+                } else {
+                    println!("{}: not found", input_type);
+                }
             }
-        } else if command.starts_with("echo") {
-            let output = &command[5..];
-            println!("{}", output);
+        } else if cmd == "echo" {
+            println!("{}", args.join(" "));
         } else {
-            println!("{}: command not found", command.trim());
+            if let Some(path) = find_executables(cmd) {
+                Command::new(&path).arg0(cmd).args(args).status().unwrap();
+            } else {
+                println!("{}: command not found", cmd);
+            }
         }
     }
 }
